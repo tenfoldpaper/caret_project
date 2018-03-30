48eed3c6a89142de94a764641471351336fa2263
tenfoldpaper <s.bien@jacobs-university.de>
2018-03-30 12:51:41 +0200

    Added random test data portion

--- CaretProjectVis.R
+++ CaretProjectVis.R
@@ -3,18 +3,26 @@ library(ggplot2)
 ## KKNN visualisation logic completeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 ui <- fluidPage(
   titlePanel("Graphs"),
-  fluidRow(
-    column(4, 
+  sidebarLayout(
+    sidebarPanel( 
            radioButtons("vsize", "Select the training sample size:",
                         c("80/20" = "eightyTwenty",
                           "64/33" = "oneThird",
-                          "50/50" = "halfHalf"))),
+                          "50/50" = "halfHalf")),
            checkboxGroupInput("vkernel", "Select kernel(s):",
                               choiceNames = list("Rectangular", "Gaussian", "Cosine"),
                               choiceValues = list("rectangular", "gaussian", "cos"),
                               selected = "rectangular"
+           ),
+           print("Test data details:"),
+           verbatimTextOutput("testData"),
+           print("Test data quality prediction:"),
+           verbatimTextOutput("testQuality"),
+           print("Test data real quality:"),
+           verbatimTextOutput("realQuality"),
+           actionButton("randomize", "Randomize data!")
     ),
-    column(8,
+    mainPanel(
            plotOutput("kknnplot1")
     )
   )
@@ -25,27 +33,55 @@ server <- function(input, output){
     #Graph variables
   vsizeget <- reactive(input$vsize)
   vkernelget <- reactive(input$vkernel)
+  
   knETdf = kknnET.train$results[kknnET.train$results$distance == 2,]
   knOTdf = kknnOT.train$results[kknnOT.train$results$distance == 2,]
   knHHdf = kknnHH.train$results[kknnHH.train$results$distance == 2,]
+  testData <- eightTwoTest.white[1,]
+  testQuality <- predict(kknnET.train, testData)
+  
+  observeEvent(input$randomize, {
+    testData <- eightTwoTest.white[sample(1:nrow(eightTwoTest.white), 1),]
+    testQuality <- predict(kknnET.train, testData)
+    cat("Button pressed!")
+    print(testData)
+    print(testQuality)
+    output$testData <- renderPrint(
+      print(testData)
+    )
+    output$testQuality <- renderPrint(
+      print(testQuality)
+    )
+    output$realQuality <- renderPrint(
+      print(testData$quality)
+    )
+  } )
+  
   output$kknnplot1 <- renderPlot(
     
     if(vsizeget() == "eightyTwenty"){
+      testData <- eightTwoTest.white[1,]
+      testQuality <- predict(kknnET.train, testData)
       ggplot(data = knETdf, aes(x = kmax, y = Accuracy)) + geom_line(data=subset(knETdf, kernel == vkernelget()[1] |
                                                                                    kernel == vkernelget()[2] |
-                                                                                   kernel == vkernelget()[3]), aes(colour = kernel)) + ylim(0.6080, 0.6110)
+                                                                                   kernel == vkernelget()[3]), aes(colour = kernel)) + ylim(0.5080, 0.6510)
     }
     else if(vsizeget() == "oneThird"){
+      testData <- oneThirdTest.white[1,]
+      testQuality <- predict(kknnOT.train, testData)
       ggplot(data = knOTdf, aes(x = kmax, y = Accuracy)) + geom_line(data=subset(knOTdf, kernel == vkernelget()[1] |
                                                                                    kernel == vkernelget()[2] |
-                                                                                   kernel == vkernelget()[3]), aes(colour = kernel)) + ylim(0.5080, 0.8110)
+                                                                                   kernel == vkernelget()[3]), aes(colour = kernel)) + ylim(0.5080, 0.6510)
     }
     else if(vsizeget() == "halfHalf"){
+      testData <- halfHalfTest.white[1,]
+      testQuality <- predict(kknnHH.train, testData)
       ggplot(data = knHHdf, aes(x = kmax, y = Accuracy)) + geom_line(data=subset(knHHdf, kernel == vkernelget()[1] |
                                                                                    kernel == vkernelget()[2] |
-                                                                                   kernel == vkernelget()[3]), aes(colour = kernel)) + ylim(0.5080, 0.8110)
+                                                                                   kernel == vkernelget()[3]), aes(colour = kernel)) + ylim(0.5080, 0.6510)
     }
   )
+  
 }
 
 shinyApp(ui=ui, server=server)
